5f4922694b97bc4df67d7a604c494907fd024de8
SOHAM BASURI <128636304+Sohambasuri-2002@users.noreply.github.com>
2023-06-23 20:40:04 +0530

    Add files via upload

---
+++ song.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Piku',
+        path: 'musics/piku.mp3',
+        artist: 'Anupam Roy',
+        cover: 'images/piku.jpg'
+    },
+    {
+        name: 'Thodi Der',
+        path: 'musics/thodider.mp3',
+        artist: 'Shreya Ghoshal',
+        cover: 'images/half.jpg'
+    },
+    {
+        name: 'Dhadak',
+        path: 'musics/dhadak.mp3',
+        artist: 'Ajay Godavale',
+        cover: 'images/dhadak.jpg'
+    },
+    {
+        name: 'Ghungroo',
+        path: 'musics/ghungroo.mp3',
+        artist: 'Arijit Singh',
+        cover: 'images/ghung.jpg'
+    },
+    {
+        name: 'Yaadoo ki almari',
+        path: 'musics/yaadon.mp3',
+        artist: 'palomi',
+        cover: 'images/yadoo.jpg'
+    },
+]
